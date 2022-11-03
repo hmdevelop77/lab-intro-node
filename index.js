29d56a43665baf462c82953a9aed4372a72156ff
Oussama Hamdani <hmweb77@gmail.com>
2022-11-03 19:00:02 +0000

    not yet

--- index.js
+++ index.js
@@ -1,32 +1,40 @@
 class SortedList {
-  
   constructor() {
     this.items = [];
     this.length = this.items.length;
-
   }
 
   add(item) {
     this.items.push(item);
-    this.items.sort(function (a,b){
-      return a -b ;
+    this.items.sort(function (a, b) {
+      return a - b;
     });
-    return this.items
+    this.length++;
   }
 
   get(pos) {
-   items.at(pos);
+    if (pos > this.items.length) {
+      return OutOfBounds;
+    } else {
+      return this.items[pos];
+    }
   }
 
   max() {
-   return this.items.max()
+    return Math.max(...this.items);
   }
 
   min() {
-    return this.items.min()
+    return Math.min(...this.items);
   }
 
-  sum() {}
+  sum() {
+    let count = 0;
+    for (i = 0; i < this.length; i++) {
+      count += this.items[i];
+    }
+    return count;
+  }
 
   avg() {}
 }
